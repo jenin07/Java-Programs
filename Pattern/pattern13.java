8de690e47e9cabdf8e8b48a02395f07ff9c53691
jenin <jeningeorge07@gmail.com>
2024-08-19 23:32:30 +0530

    java progs

--- Pattern/pattern13.java
+++ Pattern/pattern13.java
@@ -10,18 +10,19 @@ public class pattern13 {
         int n = 5;
         int k = 1;
 
-        for (int i=0; i<n; i++){
+        for (int i=1; i<n; i++){
 
             for(int j=0; j<n-i; j++){
                 System.out.print(" ");
             }
             for (int j=0; j<i; j++){
-                System.out.print(k);
-                k++;
+                System.out.print(k); //1
+                k++; //2
             }
 
-            k=i+1;
+            k=i+1; //1
             System.out.println();
         }
     }
 }
+
