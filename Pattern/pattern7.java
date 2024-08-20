5c6612f75377ff5a4b0959c9b65b20667882dd03
jenin <jeningeorge07@gmail.com>
2024-08-20 22:03:12 +0530

    java progs

--- Pattern/pattern7.java
+++ Pattern/pattern7.java
@@ -14,6 +14,36 @@ public class pattern7 {
     public static void main(String[] args){
         int n = 4 ;
 
+        // for (int i=1; i<=n; i++){
+
+        //     for (int j=0; j<i; j++){
+        //         char k='*';
+        //         if(j%2==0){
+        //             System.out.print(" "+k+" ");
+        //         }
+        //         else{
+        //             k=' ';
+        //             System.out.print(" "+k+" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // for (int i=n-1; i>1; i--){
+
+        //     for (int j=0; j<i; j++){
+        //         char k='*';
+        //         if(j%2==0){
+        //             System.out.print(" "+k+" ");
+        //         }
+        //         else{
+        //             k=' ';
+        //             System.out.print("  "+k);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
         for(int i = 0; i < n; i++){
 
             for(int j = 0; j < i; j++){
